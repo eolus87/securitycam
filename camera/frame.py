f6a357e2fa181d6e53db579c15a2ddb341e6282b
eolus87 <eolus87@hotmail.com>
2022-05-29 00:32:16 +0100

    Added a camerahandler class and a frame dataclass

---
+++ camera/frame.py
@@ -0,0 +1,32 @@
+# Personal project
+__author__ = "Eolus"
+
+# Standard libraries
+import datetime
+from dataclasses import dataclass, field
+import numpy as np
+# Third party libraries
+# Custom libraries
+
+
+@dataclass
+class Frame:
+    values: np.array = field(init=True, repr=True)
+    cam_name: str = field(init=True, repr=True)
+    frame_number: int = field(default=None)
+
+    time_stamp: datetime.datetime = field(init=False, repr=True)
+    time_stamp_str: str = field(init=False, repr=True)
+
+    horizontal_resolution: int = field(init=False, repr=True)
+    vertical_resolution: int = field(init=False, repr=True)
+    color_bands: int = field(init=False, repr=True)
+
+    def __post_init__(self):
+        # Time stamp
+        self.time_stamp = datetime.datetime.now()
+        self.time_stamp_str = self.time_stamp.strftime("%y%m%d_%H%M%S")
+
+        # Picture properties
+        self.vertical_resolution, self.horizontal_resolution, self.color_bands = self.values.shape
+
---
+++ main_new.py
@@ -0,0 +1,19 @@
+# Personal project
+__author__ = "Eolus"
+
+# Standard libraries
+import os
+import yaml
+# Third party libraries
+# Custom libraries
+from camera.camerahandler import CameraHandler
+
+
+cam_name = "_ipcam_1"
+
+with open(os.path.join("camera_confs", f"{cam_name}.yaml")) as f:
+    camera_details = yaml.load(f, Loader=yaml.FullLoader)
+
+camera_handler = CameraHandler(camera_details['address'], camera_details['name'], 30, True)
+
+camera_handler.run()
