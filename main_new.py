c2e734f40af2389cbfc9413d9707df859ea4f76e
eolus87 <eolus87@hotmail.com>
2022-05-31 22:32:51 +0100

    Modified the CameraHandler to just tackle videos. It seems that if what you require is a picture mjpeg then urllib is required. I thought VideoCapture would handle pictures as well.

--- main_new.py
+++ main_new.py
@@ -6,7 +6,7 @@ import os
 import yaml
 # Third party libraries
 # Custom libraries
-from camera.camerahandler import CameraHandler
+from camera.camerahandlervideo import CameraHandlerVideo
 
 
 cam_name = "_ipcam_1"
@@ -14,6 +14,6 @@ cam_name = "_ipcam_1"
 with open(os.path.join("camera_confs", f"{cam_name}.yaml")) as f:
     camera_details = yaml.load(f, Loader=yaml.FullLoader)
 
-camera_handler = CameraHandler(camera_details['address'], camera_details['name'], 30, True)
+camera_handler = CameraHandlerVideo(camera_details['address'], camera_details['name'], 30, True)
 
 camera_handler.run()
