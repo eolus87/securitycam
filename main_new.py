998d781fe4a1606562f922cfe68cd6d8ea189686
eolus87 <eolus87@hotmail.com>
2022-06-14 22:31:44 +0100

    Included an extra cam and did some testing. It seems it works. I will need an orchestrator for serving a random number of cameras based on the configuration files available.

--- main_new.py
+++ main_new.py
@@ -14,34 +14,52 @@ from camera.camerahandlervideo import CameraHandlerVideo
 from interfaces.fileinterface import FileInterface
 
 # %% User configuration
-cam_name = "_ipcam_1"
+cam_name_1 = "_ipcam_1"
+cam_name_2 = "_ipcam_nexus_ipwebcam"
 
 # %% Initialization and configuration
 pictures_queue = queue.Queue()
 
-with open(os.path.join("camera_confs", f"{cam_name}.yaml")) as f:
-    camera_details = yaml.load(f, Loader=yaml.FullLoader)
+with open(os.path.join("camera_confs", f"{cam_name_1}.yaml")) as f:
+    camera_details_1 = yaml.load(f, Loader=yaml.FullLoader)
+
+with open(os.path.join("camera_confs", f"{cam_name_2}.yaml")) as f:
+    camera_details_2 = yaml.load(f, Loader=yaml.FullLoader)
 
 # %% Initialization of classes
 
-camera_handler = CameraHandlerVideo(camera_details['address'], camera_details['name'], 30, True, pictures_queue)
-picture_file_interface = FileInterface(camera_details['name'], camera_details['store_path'])
+camera_handler_1 = CameraHandlerVideo(camera_details_1['address'], 
+                                      camera_details_1['name'], 
+                                      camera_details_1['frames_per_minute'], 
+                                      True, pictures_queue)
+picture_file_interface_1 = FileInterface(camera_details_1['name'], 
+                                         camera_details_1['store_path'])
+
+camera_handler_2 = CameraHandlerVideo(camera_details_2['address'], 
+                                      camera_details_2['name'], 
+                                      camera_details_2['frames_per_minute'], 
+                                      True, pictures_queue)
+picture_file_interface_2 = FileInterface(camera_details_2['name'], 
+                                         camera_details_2['store_path'])
 
 
 # %% Thread creation and While Loop
 
-threading.Thread(target=camera_handler.run, daemon=True).start()
+threading.Thread(target=camera_handler_1.run, daemon=True).start()
+threading.Thread(target=camera_handler_2.run, daemon=True).start()
 
 try:
     while True:
         print(f"{datetime.datetime.now().strftime('%y%m%d_%H%M%S')} Waiting for a picture")
         new_frame = pictures_queue.get()
         print(f"{datetime.datetime.now().strftime('%y%m%d_%H%M%S')} Picture detected")
-        picture_file_interface.store(new_frame)
-        time.sleep(0.01)
+        picture_file_interface_1.store(new_frame)
+        picture_file_interface_2.store(new_frame)
+        time.sleep(0.02)
     
 except KeyboardInterrupt:
     print("Keyboard interrup detected, closing the connection to cam.")
-    camera_handler.keep_running = False
+    camera_handler_1.keep_running = False
+    camera_handler_2.keep_running = False
     time.sleep(5)
 
