dbabd47c19bf326c650197a63d68d3bcf08cd16f
eolus87 <niguvaz@gmail.com>
2023-07-26 22:59:47 +0100

    I made it work for the moment with 1 camera. This requires some more time.

--- main_new.py
+++ main_new.py
@@ -15,7 +15,7 @@ from interfaces.fileinterface import FileInterface
 
 # %% User configuration
 cam_name_1 = "_ipcam_1"
-cam_name_2 = "_ipcam_nexus_ipwebcam"
+# cam_name_2 = "_ipcam_nexus_ipwebcam"
 
 # %% Initialization and configuration
 pictures_queue = queue.Queue()
@@ -23,8 +23,8 @@ pictures_queue = queue.Queue()
 with open(os.path.join("camera_confs", f"{cam_name_1}.yaml")) as f:
     camera_details_1 = yaml.load(f, Loader=yaml.FullLoader)
 
-with open(os.path.join("camera_confs", f"{cam_name_2}.yaml")) as f:
-    camera_details_2 = yaml.load(f, Loader=yaml.FullLoader)
+# with open(os.path.join("camera_confs", f"{cam_name_2}.yaml")) as f:
+#     camera_details_2 = yaml.load(f, Loader=yaml.FullLoader)
 
 # %% Initialization of classes
 
@@ -35,18 +35,18 @@ camera_handler_1 = CameraHandlerVideo(camera_details_1['address'],
 picture_file_interface_1 = FileInterface(camera_details_1['name'], 
                                          camera_details_1['store_path'])
 
-camera_handler_2 = CameraHandlerVideo(camera_details_2['address'], 
-                                      camera_details_2['name'], 
-                                      camera_details_2['frames_per_minute'], 
-                                      True, pictures_queue)
-picture_file_interface_2 = FileInterface(camera_details_2['name'], 
-                                         camera_details_2['store_path'])
+# camera_handler_2 = CameraHandlerVideo(camera_details_2['address'],
+#                                       camera_details_2['name'],
+#                                       camera_details_2['frames_per_minute'],
+#                                       True, pictures_queue)
+# picture_file_interface_2 = FileInterface(camera_details_2['name'],
+#                                          camera_details_2['store_path'])
 
 
 # %% Thread creation and While Loop
 
 threading.Thread(target=camera_handler_1.run, daemon=True).start()
-threading.Thread(target=camera_handler_2.run, daemon=True).start()
+# threading.Thread(target=camera_handler_2.run, daemon=True).start()
 
 try:
     while True:
@@ -54,12 +54,12 @@ try:
         new_frame = pictures_queue.get()
         print(f"{datetime.datetime.now().strftime('%y%m%d_%H%M%S')} Picture detected")
         picture_file_interface_1.store(new_frame)
-        picture_file_interface_2.store(new_frame)
+        # picture_file_interface_2.store(new_frame)
         time.sleep(0.02)
     
 except KeyboardInterrupt:
     print("Keyboard interrup detected, closing the connection to cam.")
     camera_handler_1.keep_running = False
-    camera_handler_2.keep_running = False
+    # camera_handler_2.keep_running = False
     time.sleep(5)
 
