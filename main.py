717069970f299c55dd5c62cdab19e1a8bf0cc808
eolus87 <niguvaz@gmail.com>
2024-02-18 20:20:39 +0000

    Reworked the main and the orchestrator.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from data_classes.cameraconf import CameraConf
 from workflow.orchestrator import Orchestrator
 
 # Constants
-DEBUGGING = False
+DEBUGGING = True
 CONFIGURATION_PATH = "camera_confs"
 CONFIGURATION_FORMAT = "yaml"
 TIME_BETWEEN_PROCESS_INITILIZATION = 1  # s
@@ -33,12 +33,15 @@ for conf_path in list_of_configurations:
 
 print("Starting the orchestrators")
 list_of_processes = []
+list_of_orchestrators = []
 try:
     with Manager() as manager:
         shared_dict = manager.dict()
         print("Launching the processes.")
         for conf_object in list_of_configuration_objects:
-            p = Process(target=Orchestrator().start, args=(conf_object, shared_dict,))
+            list_of_orchestrators.append(Orchestrator())
+            p = Process(target=list_of_orchestrators[-1].start, args=(conf_object, shared_dict,))
+            p.daemon = True
             p.start()
             list_of_processes.append(p)
             time.sleep(TIME_BETWEEN_PROCESS_START)
@@ -54,15 +57,18 @@ try:
                             if cv2.waitKey(1) & 0xFF == ord('q'):
                                 break
                     except Exception as e:
-                        print(f"An error occurred: {e}")
-
-            time.sleep(0.15)
+                        print(f"An error occurred while visualizing: {e}")
+                time.sleep(0.15)
+            else:
+                time.sleep(1)
 
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f"An error occurred while starting the orchestrators: {e}")
 
 finally:
     if DEBUGGING:
         cv2.destroyAllWindows()
-    for p in list_of_processes:
+    for i, p in enumerate(list_of_processes):
+        list_of_orchestrators[i].stop()
         p.join()
+        p.close()
--- workflow/orchestrator.py
+++ workflow/orchestrator.py
@@ -15,16 +15,34 @@ from processing.peopledetector import PeopleDetector
 
 class Orchestrator:
     """This class is used to orchestrate the different services."""
-    def initialization(self, camera_conf: CameraConf) -> None:
-        """Initializes the class.
+    def __init__(self):
+        """Initializes the class. It is done just for sanity with the variables that will
+        be populated later in the initialization method
+        """
+        self.__logger = None
+        self.__file_video_stream = None
+        self.__motion_detector = None
+        self.__people_detector = None
+        self.__file_interface = None
+        self.__keep_running = True
+        self.__camera_name = ""
+
+    def initialization(self, camera_conf: CameraConf, shared_dict: dict) -> None:
+        """Initializes the class. It starts the different services.
+        This method is not intended to be called directly. It is called by the start method.
+        It should have been an __init__ method, but it is not possible due to a limitation
+        with the multiprocessing library.
 
         :param camera_conf: CameraConf object with the camera configuration.
+        :param shared_dict: Dictionary to share the frames between processes.
         """
         # Initialization of logger
         self.__logger = init_logger(camera_conf.generic_conf.name)
 
         # Initialization of services
         self.__file_video_stream = FileVideoStream(camera_conf)
+        frame_object = self.__file_video_stream.read()
+        shared_dict[frame_object.cam_name] = np.copy(frame_object.values)
 
         self.__motion_detector = MotionDetector(camera_conf)
         self.__people_detector = PeopleDetector(camera_conf)
@@ -32,45 +50,44 @@ class Orchestrator:
         self.__file_interface = FileInterface(camera_conf)
 
         # Other variables
-        self.__keep_running = True
         self.__camera_name = camera_conf.generic_conf.name
 
     def start(self, camera_conf: CameraConf, shared_dict: dict) -> None:
         """Starts the flow of the camera.
         This method starts the video stream and the different services.
         """
-        self.initialization(camera_conf)
+        self.initialization(camera_conf, shared_dict)
 
         self.__file_video_stream.start()
 
         while self.__keep_running:
-            tic = time.time()
-            frame_object = self.__file_video_stream.read()
-            cam_name = frame_object.cam_name
-            frame_values = np.copy(frame_object.values)
-            shared_dict[cam_name] = frame_values
+            if self.__file_video_stream.more():
+                # tic = time.time()
+                frame_object = self.__file_video_stream.read()
+                shared_dict[frame_object.cam_name] = np.copy(frame_object.values)
 
-            motion_detected, frame_with_detection = \
-                self.__motion_detector.detect_motion(frame_object)
-            people_detected, frame_with_people_detection = \
-                self.__people_detector.detect_people(frame_object)
-            if motion_detected:
-                self.__file_interface.store(frame_with_detection, "motion")
-            if people_detected:
-                self.__file_interface.store(frame_with_people_detection, "people")
+                motion_detected, frame_with_detection = \
+                    self.__motion_detector.detect_motion(frame_object)
+                people_detected, frame_with_people_detection = \
+                    self.__people_detector.detect_people(frame_object)
+                if motion_detected:
+                    self.__file_interface.store(frame_with_detection, "motion")
+                if people_detected:
+                    self.__file_interface.store(frame_with_people_detection, "people")
 
-            toc = time.time() - tic
-            print(f"{self.__camera_name}: Frame time {toc:.2f} s")
+                # toc = time.time() - tic
+                # print(f"{self.__camera_name}: Frame time {toc:.2f} s")
+            else:
+                time.sleep(0.02)
 
-    def stop(self, time_out: int = 1) -> None:
+    def stop(self, time_out: int = 0) -> None:
         """Stops the flow of the camera.
         This method stops the video stream and the different services.
 
         :param time_out: Time to wait before exiting the flow.
         """
-        self.__file_video_stream.stop()
+        if self.__file_video_stream is not None:
+            self.__file_video_stream.stop()
         self.__keep_running = False
         time.sleep(time_out)
         self.__logger.info(f"Exiting the flow of camera: {self.__camera_name}.")
-
-
